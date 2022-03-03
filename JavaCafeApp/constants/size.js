c8591c3c85e63b9121e6dc1c7be73571c15b84d3
beingMo <beingMo001@gmail.com>
2022-03-03 18:46:08 -0500

    renamed file again

---
+++ JavaCafeApp/constants/size.js
@@ -0,0 +1,15 @@
+/**
+ * Centralizes Font Sizes
+ * @authors Mo Auguste
+ * @date    2022-03-02 21:29:11
+ * @version 1.0.0
+ */
+
+
+
+export default {
+    headingFontSize: 30,
+    subHeadingFontSize: 25,
+    paragraphFontSize: 20,
+    smallPrintFontSize: 12
+}
