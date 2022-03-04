a96480a75ed1bb4d9566eac3df255f63d07a5f7f
beingMo <beingMo001@gmail.com>
2022-03-03 19:17:55 -0500

    finished for now

--- JavaCafeApp/components/TopNavigtion.js
+++ JavaCafeApp/components/TopNavigtion.js
@@ -9,21 +9,44 @@
 import React from 'react';
 import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import colors from '../constants/colors';
+import size from '../constants/size';
 
 const TopNavigation = () => {
-    <View style={styles.topNavigationContainer}>
-        <TouchableWithoutFeedback>
-            <Ionicons name='menu-sharp'></Ionicons>
-        </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback>
-            <Ionicons name='person-sharp'></Ionicons>
-        </TouchableWithoutFeedback>
-    </View>
-}
-
+    return (
+        <View style={styles.topNavigationContainer}>
+            <TouchableWithoutFeedback>
+                <Ionicons style={styles.menuIcon} name='menu-sharp'></Ionicons>
+            </TouchableWithoutFeedback>
+            <View style={styles.accountIconContainer}>
+                <TouchableWithoutFeedback>
+                    <Ionicons style={styles.accountIcon} name='person-sharp'></Ionicons>
+                </TouchableWithoutFeedback>
+            </View>
+        </View>
+    )
+};
+/**
+ * Refactor Code: want to determine how to use flex appropriately, in order to
+ * avoid using margin
+ */
 const styles = StyleSheet.create({
     topNavigationContainer: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginVertical: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    menuIcon: {
+        color: colors.secondary,
+        fontSize: size.menuItems
+    },
+    accountIconContainer: {
+        marginLeft: 260
+    },
+    accountIcon: {
+        color: colors.secondary,
+        fontSize: size.menuItems
     }
 })
 
