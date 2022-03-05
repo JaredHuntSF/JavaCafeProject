ea063f27a299ad1facedbe568e8edb92c0a6a76e
beingMo <beingMo001@gmail.com>
2022-03-04 22:25:02 -0500

    adding regex

--- JavaCafeApp/screens/AccountRegistration.js
+++ JavaCafeApp/screens/AccountRegistration.js
@@ -6,62 +6,102 @@
  * @version 1.0.0
  */
 
-import React from 'react';
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
-import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
-import { clearErrors } from 'react-native/Libraries/LogBox/Data/LogBoxData';
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    Button,
+    StyleSheet,
+    TextInput,
+    TouchableWithoutFeedback,
+    Keyboard,
+    ScrollView
+} from 'react-native';
 
 import colors from '../constants/colors';
 import size from '../constants/size';
 
 const AccountRegistration = () => {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const nameHandler = input => {
+        //setName(input.replace(/^[^a-zA-Z]+(([',. -][a-zA-Z ])?[^a-zA-Z]*)*$/g, ''));
+        //setName(input.replace(/^[^a-zA-Z]+([',. -][a-zA-Z ])?[^a-zA-Z]*)*$/g, ''));
+    };
+    const emailHandler = input => {
+
+    };
+    const passwordHandler = input => {
+
+    };
+    const passwordCheckHandler = input => {
+
+    };
+    const phoneHandler = input => {
+        setPhone(input.replace(/[^0-9]/g, ''));
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.header}>Account Registration</Text>
-            <View style={styles.body}>
-                <Text style={styles.subHeader}>Name</Text>
-                <TextInput style={styles.input}
-                    placeholder="Enter your name"
-                    placeholderTextColor={colors.text}
-                    textContentType="name"
-                    blurOnSubmit={true}
-                    maxLength={20} />
-                <Text style={styles.subHeader}>Email</Text>
-                <TextInput style={styles.input}
-                    placeholder="Enter your email"
-                    placeholderTextColor={colors.text}
-                    textContentType="emailAddress"
-                    blurOnSubmit={true}
-                    maxLength={50}
-                    keyboardType="email-address" />
-                <Text style={styles.subHeader}>Password</Text>
-                <TextInput style={styles.input}
-                    placeholder="Enter a password"
-                    placeholderTextColor={colors.text}
-                    textContentType="password"
-                    blurOnSubmit={true}
-                    maxLength={20} />
-                <Text style={styles.subHeader}>Retype Password</Text>
-                <TextInput style={styles.input}
-                    placeholder="Enter your password"
-                    placeholderTextColor={colors.text}
-                    textContentType="password"
-                    blurOnSubmit={true}
-                    maxLength={20} />
-                <Text style={styles.subHeader}>Phone Number</Text>
-                <TextInput style={styles.input}
-                    placeholder="+1 (XXX) XXX - XXXX"
-                    placeholderTextColor={colors.text}
-                    textContentType="telephoneNumber"
-                    blurOnSubmit={true}
-                    maxLength={10}
-                    keyboardType="numeric" />
-                <View style={styles.button}>
-                    <Button title="Sign Up" color={colors.secondary} />
-                </View>
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.header}>Account Registration</Text>
+                <ScrollView>
+                    <View style={styles.body}>
+                        <Text style={styles.subHeader}>Name</Text>
+                        <TextInput style={styles.input}
+                            placeholder="Enter your name"
+                            placeholderTextColor={colors.text}
+                            textContentType="name"
+                            blurOnSubmit={true}
+                            maxLength={20}
+                            onChangeText={nameHandler}
+                            value={name} />
+                        <Text style={styles.subHeader}>Email</Text>
+                        <TextInput style={styles.input}
+                            placeholder="Enter your email"
+                            placeholderTextColor={colors.text}
+                            textContentType="emailAddress"
+                            blurOnSubmit={true}
+                            maxLength={50}
+                            keyboardType="email-address" />
+                        <Text style={styles.subHeader}>Password</Text>
+                        <TextInput style={styles.input}
+                            placeholder="Enter a password"
+                            placeholderTextColor={colors.text}
+                            textContentType="password"
+                            blurOnSubmit={true}
+                            maxLength={20} />
+                        <Text style={styles.subHeader}>Retype Password</Text>
+                        <TextInput style={styles.input}
+                            placeholder="Enter your password"
+                            placeholderTextColor={colors.text}
+                            textContentType="password"
+                            blurOnSubmit={true}
+                            maxLength={20} />
+                        <Text style={styles.subHeader}>Phone Number</Text>
+                        <TextInput style={styles.input}
+                            placeholder="+1 (XXX) XXX - XXXX"
+                            placeholderTextColor={colors.text}
+                            textContentType="telephoneNumber"
+                            blurOnSubmit={true}
+                            maxLength={10}
+                            keyboardType="numeric"
+                            onChangeText={phoneHandler}
+                            value={phone} />
+                        <View style={styles.button}>
+                            <Button title="Sign Up" color={colors.secondary} />
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     )
 
 }
