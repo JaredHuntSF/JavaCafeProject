f543ff4db0a60145a7a58d6a4abd9d689ac5a89b
JaredHuntSF <jaredhunt35@yahoo.com>
2022-03-11 19:55:31 -0500

    dummy data tweak

--- JavaCafeApp/data/dummy-data.js
+++ JavaCafeApp/data/dummy-data.js
@@ -3,7 +3,7 @@ import Product from '../models/product';
 const PRODUCTS = [
   new Product(
     'p1',
-    'u1',
+    'hot',
     'Red Shirt',
     'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     'A red t-shirt, perfect for days with non-red weather.',
@@ -11,7 +11,7 @@ const PRODUCTS = [
   ),
   new Product(
     'p2',
-    'u1',
+    'hot',
     'Blue Carpet',
     'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     'Fits your red shirt perfectly. To stand on. Not to wear it.',
@@ -19,7 +19,7 @@ const PRODUCTS = [
   ),
   new Product(
     'p3',
-    'u2',
+    'cold',
     'Coffee Mug',
     'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
     'Can also be used for tea!',
@@ -27,7 +27,7 @@ const PRODUCTS = [
   ),
   new Product(
     'p4',
-    'u3',
+    'cold',
     'The Book - Limited Edition',
     'https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg',
     "What the content is? Why would that matter? It's a limited edition!",
@@ -35,7 +35,7 @@ const PRODUCTS = [
   ),
   new Product(
     'p5',
-    'u3',
+    'pastry',
     'PowerBook',
     'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
     'Awesome hardware, and a hefty price. Buy now before a new one is released!',
@@ -43,7 +43,7 @@ const PRODUCTS = [
   ),
   new Product(
     'p6',
-    'u1',
+    'pastry',
     'Pen & Paper',
     'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
     "Useful for those who want to take notes.",
