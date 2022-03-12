22e1caadc9adcce40c02a78152ae45073bbbae55
bardizzone1 <98251184+bardizzone1@users.noreply.github.com>
2022-03-11 21:52:41 -0500

    returning lost screen

--- JavaCafeApp/components/IndividualPastOrders.js
+++ JavaCafeApp/components/IndividualPastOrders.js
@@ -6,21 +6,97 @@
  * @version 1.0.0
  */
 
-import React from 'react';
-import { View, Text, Modal, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Modal, StyleSheet, FlatList, Text, TouchableWithoutFeedback } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-const IndividualPastOrders = () => {
+import colors from '../constants/colors';
+import size from '../constants/size';
+
+const IndividualPastOrders = props => {
+
+    //Refactor: Get from db
+    const [orderNumber, setOrderNumber] = useState('57');
+    const [selectedOrder, setSelectedOrder] = useState([{
+        key: 'Large Coffee', value: 'Large Coffee',
+        key: 'Cheese & Fruit Box', value: 'Cheese & Fruit Box',
+        key: 'Ham & Swiss Sandwich', value: 'Ham & Swiss Sandwich',
+        key: 'Kettle Chips', value: 'Kettle Chips'
+    }]);
+    const [subtotal, setSubtotal] = useState('X.XX');
+    const [tax, setTax] = useState('X.XX');
+    const [total, setTotal] = useState('X.XX');
+
+    //Refactor: Get from db
+    const addToArrayHandler = () => {
+        setSelectedOrder();
+    };
+    const fetchSubtotalHandler = () => {
+        setSubtotal();
+    }
+    const fetchTax = () => {
+        setTax();
+    }
+    const fetchTotal = () => {
+        setTotal();
+    }
+
     return (
-        <Modal>
-            <View>
-
+        <Modal visible={props.visible}>
+            <View style={styles.screen}>
+                <View style={styles.row}>
+                    <Text style={styles.header}>Order# {orderNumber}</Text>
+                    <TouchableWithoutFeedback onPress={props.onClose}>
+                        <Ionicons style={styles.closeIcon} name='close-outline' />
+                    </TouchableWithoutFeedback>
+                </View>
+                <View style={styles.orderItemsContainer}>
+                    <FlatList data={selectedOrder} renderItem={orderItem => <Text style={styles.listItems}>{orderItem.item.value}</Text>} />
+                </View>
+                <View style={styles.orderItemsContainer}>
+                    <View style={styles.row}>
+                        <Text style={styles.listItems}>Subtotal</Text>
+                        <Text style={styles.listItems}>{subtotal}</Text>
+                    </View>
+                    <View style={styles.row}>
+                        <Text style={styles.listItems}>Tax</Text>
+                        <Text style={styles.listItems}>{tax}</Text>
+                    </View>
+                    <View style={styles.row}>
+                        <Text style={styles.listItems}>Total</Text>
+                        <Text style={styles.listItems}>{total}</Text>
+                    </View>
+                </View>
             </View>
         </Modal>
     );
 };
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        marginTop: 20,
+        padding: 20,
+        backgroundColor: colors.primary
+    },
+    header: {
+        fontSize: size.headingFontSize,
+        color: colors.text
+    },
+    orderItemsContainer: {
+        marginTop: 20
+    },
+    listItems: {
+        color: colors.text
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    closeIcon: {
+        color: colors.text,
+        fontSize: size.menuItems
+    }
 });
 
 export default IndividualPastOrders;
