af0347910e5d6fffe7ca8e88858f4dbdbd2540c9
JaredHuntSF <jaredhunt35@yahoo.com>
2022-03-13 18:55:08 -0400

    Fixed bottom navigation overlapping

--- JavaCafeApp/screens/Menu.js
+++ JavaCafeApp/screens/Menu.js
@@ -26,141 +26,58 @@ const Menu = props => {
 
 
         <View style={styles.container}>
-            <View style={styles.filter}>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Menu</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Hot Brew</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Cold Brew</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Pastries</Text>
-                </TouchableOpacity>
+            <View style={styles.contentcontainer}>
+                <View style={styles.filter}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Menu</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Hot Brew</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Cold Brew</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Pastries</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <FlatList horizontal
+                data={products}
+                keyExtractor={item => item.id}
+                renderItem={itemData => 
+                
+                <View style={styles.subMenuItem}>
+                    <TouchableOpacity onPress={() => {props.navigation.navigate('ProductSelection', {productId: itemData.item.id, productTitle: itemData.item.title})}}>
+                        <Image style={styles.image} source={{uri:itemData.item.imageUrl}}></Image>
+                        <Text style={styles.text}>{itemData.item.title}</Text>
+                    </TouchableOpacity>
+                </View>}/>
+                <FlatList horizontal
+                data={cold}
+                keyExtractor={item => item.id}
+                renderItem={itemData => 
+                <View style={styles.subMenuItem}>
+                    <TouchableOpacity onPress={() => {props.navigation.navigate('ProductSelection', {productId: itemData.item.id, productTitle: itemData.item.title})}}>
+                        <Image style={styles.image} source={{uri:itemData.item.imageUrl}}></Image>
+                        <Text style={styles.text}>{itemData.item.title}</Text>
+                    </TouchableOpacity>
+                </View>}/>
+                <FlatList horizontal
+                data={pastries}
+                keyExtractor={item => item.id}
+                renderItem={itemData => 
+                <View style={styles.subMenuItem}>
+                    <TouchableOpacity onPress={() => {props.navigation.navigate('ProductSelection', {productId: itemData.item.id, productTitle: itemData.item.title})}}>
+                        <Image style={styles.image} source={{uri:itemData.item.imageUrl}}></Image>
+                        <Text style={styles.text}>{itemData.item.title}</Text>
+                    </TouchableOpacity>
+                </View>}/>
             </View>
-
-            <FlatList horizontal
-            data={products}
-            keyExtractor={item => item.id}
-            renderItem={itemData => 
             
-            <View style={styles.subMenuItem}>
-                <TouchableOpacity onPress={() => {props.navigation.navigate('ProductSelection', {productId: itemData.item.id, productTitle: itemData.item.title})}}>
-                    <Image style={styles.image} source={{uri:itemData.item.imageUrl}}></Image>
-                    <Text style={styles.text}>{itemData.item.title}</Text>
-                </TouchableOpacity>
-            </View>}/>
-            <FlatList horizontal
-            data={cold}
-            keyExtractor={item => item.id}
-            renderItem={itemData => 
-            <View style={styles.subMenuItem}>
-                <TouchableOpacity onPress={() => {props.navigation.navigate('ProductSelection', {productId: itemData.item.id, productTitle: itemData.item.title})}}>
-                    <Image style={styles.image} source={{uri:itemData.item.imageUrl}}></Image>
-                    <Text style={styles.text}>{itemData.item.title}</Text>
-                </TouchableOpacity>
-            </View>}/>
-            <FlatList horizontal
-            data={pastries}
-            keyExtractor={item => item.id}
-            renderItem={itemData => 
-            <View style={styles.subMenuItem}>
-                <TouchableOpacity onPress={() => {props.navigation.navigate('ProductSelection', {productId: itemData.item.id, productTitle: itemData.item.title})}}>
-                    <Image style={styles.image} source={{uri:itemData.item.imageUrl}}></Image>
-                    <Text style={styles.text}>{itemData.item.title}</Text>
-                </TouchableOpacity>
-            </View>}/>
-
-            
-
-            {/* 
-            <ScrollView horizontal style={styles.subMenu}  color={colors.assessory} >
-                <MenuItem />
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/iced_coffee.jpg')} style={styles.subMenuImg} />
-                    <Text >Iced Coffee</Text> 
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/iced_latte.jpg')} style={styles.subMenuImg} />
-                    <Text>Iced Latte</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/iced_mocha.jpg')} style={styles.subMenuImg} />
-                    <Text>Iced Mocha</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/iced_tea.jpg')} style={styles.subMenuImg} />
-                    <Text>Iced Tea</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/lemonade.jpg')} style={styles.subMenuImg} />
-                    <Text>Lemonade</Text>
-                </TouchableOpacity>
-
-            </ScrollView >
-
-            <ScrollView horizontal style={styles.subMenu}>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/coffee.jpg')} style={styles.subMenuImg} />
-                    <Text>Classic Coffee</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/decaf_coffee.jpg')} style={styles.subMenuImg} />
-                    <Text>Decaf Coffee</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/cappucino.jpg')} style={styles.subMenuImg} />
-                    <Text>Cappuccino</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/espresso.jpg')} style={styles.subMenuImg} />
-                    <Text>Espresso</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/latte.jpg')} style={styles.subMenuImg} />
-                    <Text>Latte</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/hot_tea.jpg')} style={styles.subMenuImg} />
-                    <Text>Hot Tea</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/hot_chocolate.jpg')} style={styles.subMenuImg} />
-                    <Text>Hot Chocolate</Text>
-                </TouchableOpacity>
-            </ScrollView >
-
-            <ScrollView horizontal style={styles.subMenu}>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/cinn_bun.jpg')} style={styles.subMenuImg} />
-                    <Text>Cinnamon Roll</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/macaron.jpg')} style={styles.subMenuImg} />
-                    <Text>Macaron</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/scone.jpg')} style={styles.subMenuImg} />
-                    <Text>Vanilla Bean Scone</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/eclair.jpg')} style={styles.subMenuImg} />
-                    <Text>Éclair</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/sugar_cookie.jpg')} style={styles.subMenuImg} />
-                    <Text>Sugar Cookie</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.subMenuItem}>
-                    <Image source={require('../assets/choco_chip_cookie.jpg')} style={styles.subMenuImg} />
-                    <Text>Chocolate Chip Cookie</Text>
-                </TouchableOpacity>
-            </ScrollView > */}
-
-    <BottomNavigation navigation={props.navigation}/>
-
+            <View style={styles.navbar}>
+                <BottomNavigation navigation={props.navigation}/>
+            </View>
         </View>
     );
 }
@@ -168,16 +85,19 @@ const Menu = props => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: colors.primary,
+    },
+    contentcontainer: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: colors.primary,
-
     },
 
     filter: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        padding: 80,
+        padding: 40,
     },
     subMenu: {
         width: 320,
@@ -210,6 +130,9 @@ const styles = StyleSheet.create({
     buttonText: {
         color: colors.text,
         fontSize: 20
+    },
+    navbar: {
+        height: 80
     }
 
 });
