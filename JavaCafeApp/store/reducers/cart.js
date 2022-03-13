f97dcae5f691fbf49489323cb7476a9e4d6ab172
JaredHuntSF <jaredhunt35@yahoo.com>
2022-03-13 19:55:00 -0400

    adding functionality into cart.js reducer

--- JavaCafeApp/store/reducers/cart.js
+++ JavaCafeApp/store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART } from "../actions/cart";
+import CartItem from "../../models/cartItem";
 
 const initialState = {
     items: {}, 
@@ -11,6 +12,28 @@ export default (state = initialState, action) => {
             const addedProduct = action.product; //Get the added product out of the action, this was set up in cart.js action file
             const prodPrice = addedProduct.price;
             const prodTitle = addedProduct.title;
+
+            if (state.items[addedProduct.id]) {
+                //Item is already in cart
+                const updatedCartItem = new CartItem(
+                    state.items[addedProduct.id].quantity + 1,
+                    prodPrice,
+                    prodTitle,
+                    state.items[addedProduct.id].sum + prodPrice
+                );
+                return {
+                    ...state, //return a copy of state
+                    items: { ...state.items, [addedProduct.id]: updatedCartItem },
+                    totalAmount: state.totalAmount + prodPrice
+                }
+            } else {
+                const newCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice)
+                return {
+                    ...state, //return a copy of state
+                    items: {...state.items, [addedProduct.id]: newCartItem },
+                    totalAmount: state.totalAmount + prodPrice
+                }
+            }
     }
     return state;
 }
