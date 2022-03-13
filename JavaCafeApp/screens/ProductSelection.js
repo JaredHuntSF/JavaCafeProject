88be565ba9a8f1c59174bb7ee1e45edbc7d1ea21
JaredHuntSF <jaredhunt35@yahoo.com>
2022-03-13 18:47:02 -0400

    Added ProductSelection Screen

---
+++ JavaCafeApp/screens/ProductSelection.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet
+} from 'react-native';
+import { useSelector } from 'react-redux';
+import BottomNavigation from '../components/BottomNavigation';
+import colors from '../constants/colors';
+
+const ProductSelection = props => {
+  const productId = props.navigation.getParam('productId');
+  const selectedProduct = useSelector(state =>
+    state.products.availableProducts.find(prod => prod.id === productId)
+  );
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.contentcontainer}>
+        <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+        <View style={styles.actions}>
+          <Button color='orange' title="Add to Cart" onPress={() => {}} />
+        </View>
+        <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+        <Text style={styles.description}>{selectedProduct.description}</Text>
+        
+      </ScrollView>
+      <View style={styles.navbar}>
+        <BottomNavigation navigation={props.navigation}/>
+      </View>
+    </View>
+  );
+};
+
+ProductSelection.navigationOptions = navData => {
+  return {
+    headerTitle: navData.navigation.getParam('productTitle')
+  };
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+    marginBottom: 25
+  
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center'
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20,
+    color: colors.text
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  contentcontainer: {
+    flex: 1
+  },
+  navbar: {
+    height: 80
+  },
+});
+
+export default ProductSelection;
