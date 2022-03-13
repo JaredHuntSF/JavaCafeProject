8ca5c986e00acdc8abdb3e85c7333cb20c0f2d35
bardizzone1 <98251184+bardizzone1@users.noreply.github.com>
2022-03-13 19:59:17 -0400

    updated home screen to correct colors

--- JavaCafeApp/constants/colors.js
+++ JavaCafeApp/constants/colors.js
@@ -9,7 +9,7 @@
 
 export default {
     primary: '#000', //Black
-    secondary: '#5F7385',
+    secondary: '#5F5C61', //Updated to match prototype color. Old color is: '#5F7385',
     header: '#C0C9D1', //Gray
     assessory: '#FF850F', //Orange
     text: '#fff' //White
