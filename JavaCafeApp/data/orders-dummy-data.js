479b4227e8cab8c57110d71bd3209b7bd7ae6412
beingMo <beingMo001@gmail.com>
2022-03-20 00:41:23 -0400

    added dummy array of sale items

--- JavaCafeApp/data/orders-dummy-data.js
+++ JavaCafeApp/data/orders-dummy-data.js
@@ -1,36 +1,41 @@
 class Order {
-    constructor(id, date, customerFirstName, customerLastName) {
+    constructor(id, date, customerFirstName, customerLastName, saleItems) {
         this.id = id;
         this.date = date;
         this.customerFirstName = customerFirstName;
         this.customerLastName = customerLastName;
+        this.saleItems = saleItems;
     }
 }
-
+const orderItems = ['Large Coffee', 'Small Coffee', 'Small Tea']
 const CUSTOMER_ORDERS = [
     new Order(
         '66',
         '03/17/22',
         'Mike',
-        'L.'
+        'L.',
+        orderItems
     ),
     new Order(
         '65',
         '03/17/22',
         'Mike',
-        'L.'
+        'L.',
+        orderItems
     ),
     new Order(
         '64',
         '03/17/22',
         'Mike',
-        'L.'
+        'L.',
+        orderItems
     ),
     new Order(
         '63',
         '03/17/22',
         'Mike',
-        'L.'
+        'L.',
+        orderItems
     )
 ];
 
