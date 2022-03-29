673b6c6a257f797773cb0368e6ddcca1689ea366
beingMo <beingMo001@gmail.com>
2022-03-28 20:43:27 -0400

    small edits

--- JavaCafeApp/store/actions/customers.js
+++ JavaCafeApp/store/actions/customers.js
@@ -10,8 +10,24 @@
 export const ADD_CUSTOMER = 'ADD_CUSTOMER';
 
 export const addCustomer = (id, customerName, email, phone, password) => {
-    return dispatch => {
-        fetch('https://java-cafe-19cf1-default-rtdb.firebaseio.com/customer');
+    return async dispatch => {
+        const response = await fetch('https://java-cafe-19cf1-default-rtdb.firebaseio.com/customer.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                customerName,
+                email,
+                phone,
+                password
+            })
+        });
+
+        const responseData = await response.json();
+
+        console.log(responseData);
+
         dispatch({
             type: ADD_CUSTOMER,
             customerData: {
