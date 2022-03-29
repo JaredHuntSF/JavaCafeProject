52fb3722a8c9ffd16ed27b2446e2836b15cbdcc0
beingMo <beingMo001@gmail.com>
2022-03-29 15:14:25 -0400

    started

---
+++ JavaCafeApp/store/reducers/customers.js
@@ -0,0 +1,22 @@
+import { ADD_CUSTOMER } from "../actions/customers";
+import Customer from '../../models/customer';
+import CUSTOMERS from '../../data/customers-dummy-data';
+
+const initialState = {
+    existingCustomers: CUSTOMERS
+};
+
+export default (state = initialState, action) => {
+    const newCustomer = new Customer(
+        new Date().toString(),
+        action.customerData.name,
+        action.customerData.email,
+        action.customerData.phone
+    );
+
+    return {
+        ...state,
+        existingCustomers
+
+    };
+}
