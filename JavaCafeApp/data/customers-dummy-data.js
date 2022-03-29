b93b38bc084f6549310770eedff50133ac7db035
beingMo <beingMo001@gmail.com>
2022-03-29 15:12:02 -0400

    dummy data for customers

---
+++ JavaCafeApp/data/customers-dummy-data.js
@@ -0,0 +1,18 @@
+const CUSTOMER = [
+    new CUSTOMER(
+        '1',
+        'Mo Auguste',
+        'mo.auguste@javacafe.com',
+        'kidsnextdoor',
+        '352123456'
+    ),
+    new CUSTOMER(
+        '2',
+        'John',
+        'Tha Client',
+        'whatsaqrcode?',
+        '1234567890'
+    )
+];
+
+export default CUSTOMER_ORDERS;
