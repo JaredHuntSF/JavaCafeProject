5bfc3fbf8e0d28b2e76f7483f3b6d626609fbccd
beingMo <beingMo001@gmail.com>
2022-04-09 15:56:54 -0400

    added simple login auth

--- JavaCafeApp/screens/Login.js
+++ JavaCafeApp/screens/Login.js
@@ -9,15 +9,50 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
 import colors from '../constants/colors';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import * as authActions from '../store/actions/auth';
 
 const testAlert = () => {
-    Alert.alert('Wrong input!', 'Please check the errors in the form.', [{text: 'Okay'}] )
+    Alert.alert('Wrong input!', 'Please check the errors in the form.', [{ text: 'Okay' }])
 };
 
 
 
 const Login = props => {
-    return(
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState();
+
+    const dispatch = useDispatch();
+
+    const loginHandler = () => {
+        //setError(null);
+        try {
+            dispatch(
+                authActions.login(
+                    email,
+                    password,
+                )
+            )
+        } catch (err) {
+            setError(err.message);
+        }
+        //props.navigation.navigate('Home')
+        console.log(email);
+        console.log(password);
+        console.log(error);
+    };
+
+    const passwordHandler = input => {
+        setPassword(input);
+    };
+
+    const emailHandler = input => {
+        setEmail(input);
+    };
+
+    return (
         <View style={styles.container} navigation={props.navigation}>
 
             <View>
@@ -26,35 +61,44 @@ const Login = props => {
 
             <View style={styles.body}>
                 <View style={styles.loginelement}>
-                <Text style={styles.body}>Email</Text>        
-                <TextInput style={styles.textinput}
-                    placeholder="Enter your email address" />
+                    <Text style={styles.body}>Email</Text>
+                    <TextInput style={styles.textinput}
+                        placeholder="Enter your email address"
+                        onChangeText={emailHandler}
+                        value={email} />
                 </View>
 
 
                 <View style={styles.loginelement}>
                     <Text style={styles.body}>Password</Text>
-                    <TextInput  style={styles.textinput}
-                    secureTextEntry={true} placeholder="Enter your password"/>
+                    <TextInput style={styles.textinput}
+                        secureTextEntry={true}
+                        placeholder="Enter your password"
+                        onChangeText={passwordHandler}
+                        value={password} />
                 </View>
 
                 <View style={styles.interactabletext}>
-                    <TouchableOpacity onPress={() => {props.navigation.navigate({ routeName: 'AccountRegistration' })}}>
-                        <Text style={styles.body}>Don't have an account? Register Now</Text>  
+                    <TouchableOpacity onPress={() => { props.navigation.navigate({ routeName: 'AccountRegistration' }) }}>
+                        <Text style={styles.body}>Don't have an account? Register Now</Text>
                     </TouchableOpacity>
-                </View> 
-            
-            
-                <View>
-                    <Button color='#FF850F' title="Log In" onPress={() => {props.navigation.navigate({ routeName: 'Home' })}} />
                 </View>
 
+                <View>
+                    <Button color='#FF850F' title="Log In" onPress={loginHandler} />
+                </View>
+
+
+                {/* <View>
+                    <Button color='#FF850F' title="Log In" onPress={() => {props.navigation.navigate({ routeName: 'Home' })}} />
+                </View> */}
+
                 <View style={styles.interactabletext}>
-                    <TouchableOpacity onPress={() => {props.navigation.navigate({ routeName: 'ForgotPassword' })}}>
+                    <TouchableOpacity onPress={() => { props.navigation.navigate({ routeName: 'ForgotPassword' }) }}>
                         <Text style={styles.body}>Forgot your password?</Text>
                     </TouchableOpacity>
                 </View>
-            </View>     
+            </View>
         </View>
     );
 };
