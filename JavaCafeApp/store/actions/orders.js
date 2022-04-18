87d625a5cfea158ecba6160b8c7adb40d9ddf426
JaredHuntSF <jaredhunt35@yahoo.com>
2022-04-17 23:30:26 -0400

    Storing orders on database

--- JavaCafeApp/store/actions/orders.js
+++ JavaCafeApp/store/actions/orders.js
@@ -1,5 +1,32 @@
 export const ADD_ORDER = 'ADD_ORDER';
-
-export const addOrder = (cartItems, totalAmount) => {
-    return {type: ADD_ORDER, orderData: {items: cartItems, amount: totalAmount}}
-}
+export const addOrder = (cartItems, totalAmount) => 
+{
+  return async dispatch => 
+  {
+    const date = new Date();
+    const response = await fetch(
+      'https://seniorprojectjavacafe-default-rtdb.firebaseio.com/orders.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString()
+        })
+      }
+    );
+    const resData = await response.json();
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date
+      }
+    });
+  };
+};
--- JavaCafeApp/store/reducers/orders.js
+++ JavaCafeApp/store/reducers/orders.js
@@ -4,17 +4,12 @@ import Order from '../../models/order';
 const initialState = {
   orders: []
 };
-
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_ORDER:
-      const newOrder = new Order(new Date().toString(),action.orderData.items,action.orderData.amount,new Date()
-      );
-      return {
-        ...state,
-        orders: state.orders.concat(newOrder)
-      };
-  }
-
+      const newOrder = new Order(action.orderData.id,action.orderData.items,action.orderData.amount,action.orderData.date);
+    return{
+      ...state,
+      orders: state.orders.concat(newOrder)};}
   return state;
 };
